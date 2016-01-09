b6a9e1934c92114bc54bbfb43721e22ff6268570
Sergio Oehler <soehler@truesys.com>
2016-01-09 15:24:28 -0200

    first commit

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -0,0 +1,32 @@
+package com.udacity.gradle.builditbigger.paid;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udacity.gradle.builditbigger.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+    public MainActivityFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main_activity, container, false);
+        return root;
+
+    }
+
+}
